e9aadcc4e9fdffa84d8b97ea226644a8ff652f2d
França Mac Dowell <fmdss@ic.ufal.br>
2017-05-06 23:12:48 -0300

    Improving CUnit

--- CP com R/CPcunitTest.c
+++ CP com R/CPcunitTest.c
@@ -1,5 +1,4 @@
 #include "heap.h"
-#include "priorityQueue.h"
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h"
 /* Pointer to the file used by the tests. */
@@ -9,7 +8,7 @@ static FILE* temp_file = NULL;
 * Opens the temporary file used by the tests.
 * Returns zero on success, non-zero otherwise.
 */
-int init_suite1(void)
+int init_suit(void)
 {
   if (NULL == (temp_file = fopen("temp.txt", "w+"))) {
     return -1;
@@ -23,7 +22,7 @@ int init_suite1(void)
 * Closes the temporary file used by the tests.
 * Returns zero on success, non-zero otherwise.
 */
-int clean_suite1(void)
+int clean_suite(void)
 {
   if (0 != fclose(temp_file)) {
     return -1;
@@ -34,177 +33,86 @@ int clean_suite1(void)
   }
 }
 
-void test_enqueueHeap(void)
+void test_enqueueMaxHeap(void)
 {
-  int i;
+  int i, j;
   int nums[4][10] = {7, 3, 2, 1, 5, 6, 9, 4, 8, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
   int expected[4][10] = {7, 7, 7, 7, 7, 7, 9, 9, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
 
-  Heap_t *heap = createHeap(10, '+');
-  for (i = 0; i < 10; i ++)
+  Heap_t *heap;
+  for (i = 0; i < 4; i ++)
   {
-    enqueueHeap(heap, nums[0][i]);
-    CU_ASSERT(frontHeap(heap) == expected[0][i]);
+    heap = createHeap(10, '+');
+    for (j = 0; j < 10; j ++)
+    {
+      enqueueHeap(heap, nums[i][j]);
+      CU_ASSERT(frontHeap(heap) == expected[i][j]);
+    }
+    destroyHeap(heap);
   }
-  destroyHeap(heap);
-
-  heap = createHeap(10, '+');
-  for (i = 0; i < 10; i ++)
-  {
-    enqueueHeap(heap, nums[1][i]);
-    CU_ASSERT(frontHeap(heap) == expected[1][i]);
-  }
-  destroyHeap(heap);
-
-  heap = createHeap(10, '+');
-  for (i = 0; i < 10; i ++)
-  {
-    enqueueHeap(heap, nums[2][i]);
-    CU_ASSERT(frontHeap(heap) == expected[2][i]);
-  }
-  destroyHeap(heap);
-
-  heap = createHeap(10, '+');
-  for (i = 0; i < 10; i ++)
-  {
-    enqueueHeap(heap, nums[3][i]);
-    CU_ASSERT(frontHeap(heap) == expected[3][i]);
-  }
-  destroyHeap(heap);
 }
 
-void test_enqueuePriorityQueue(void)
+void test_dequeueMaxHeap(void)
 {
-  int i;
-  int nums[4][10] = {7, 3, 2, 1, 5, 6, 9, 4, 8, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
-  int expected[4][10] = {7, 7, 7, 7, 7, 7, 9, 9, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
-
-  PriorityQueue_t *pq = newPriorityQueue();
-  for (i = 0; i < 10; i ++)
-  {
-    enqueuePriorityQueue(pq, nums[0][i], nums[0][i]);
-    CU_ASSERT(maximum(pq) == expected[0][i]);
-  }
-  destroyPriorityQueue(pq);
-
-  pq = newPriorityQueue();
-  for (i = 0; i < 10; i ++)
-  {
-    enqueuePriorityQueue(pq, nums[1][i], nums[1][i]);
-    CU_ASSERT(maximum(pq) == expected[1][i]);
-  }
-  destroyPriorityQueue(pq);
-
-  pq = newPriorityQueue();
-  for (i = 0; i < 10; i ++)
-  {
-    enqueuePriorityQueue(pq, nums[2][i], nums[2][i]);
-    CU_ASSERT(maximum(pq) == expected[2][i]);
-  }
-  destroyPriorityQueue(pq);
-
-  pq = newPriorityQueue();
-  for (i = 0; i < 10; i ++)
-  {
-    enqueuePriorityQueue(pq, nums[3][i], nums[3][i]);
-    CU_ASSERT(maximum(pq) == expected[3][i]);
-  }
-  destroyPriorityQueue(pq);
-}
-
-void test_dequeueHeap(void)
-{
-  int i;
+  int i, j;
   int nums[4][10] = {7, 3, 2, 1, 5, 6, 9, 4, 8, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
   int expected[4][10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
 
-  Heap_t *heap = createHeap(10, '+');
-  for (i = 0; i < 10; i ++)
-    enqueueHeap(heap, nums[0][i]);
-  for (i = 0; i < 10; i ++)
+  Heap_t *heap;
+  for (i = 0; i < 4; i ++)
   {
-    CU_ASSERT(frontHeap(heap) == expected[0][i]);
-    dequeueHeap(heap);
+    heap = createHeap(10, '+');
+    for (j = 0; j < 10; j ++)
+      enqueueHeap(heap, nums[i][j]);
+    for (j = 0; j < 10; j ++)
+    {
+      CU_ASSERT(frontHeap(heap) == expected[i][j]);
+      dequeueHeap(heap);
+    }
+    destroyHeap(heap);
   }
-  destroyHeap(heap);
-
-  heap = createHeap(10, '+');
-  for (i = 0; i < 10; i ++)
-    enqueueHeap(heap, nums[1][i]);
-  for (i = 0; i < 10; i ++)
-  {
-    CU_ASSERT(frontHeap(heap) == expected[1][i]);
-    dequeueHeap(heap);
-  }
-  destroyHeap(heap);
-
-  heap = createHeap(10, '+');
-  for (i = 0; i < 10; i ++)
-    enqueueHeap(heap, nums[2][i]);
-  for (i = 0; i < 10; i ++)
-  {
-    CU_ASSERT(frontHeap(heap) == expected[2][i]);
-    dequeueHeap(heap);
-  }
-  destroyHeap(heap);
-
-  heap = createHeap(10, '+');
-  for (i = 0; i < 10; i ++)
-    enqueueHeap(heap, nums[3][i]);
-  for (i = 0; i < 10; i ++)
-  {
-    CU_ASSERT(frontHeap(heap) == expected[3][i]);
-    dequeueHeap(heap);
-  }
-  destroyHeap(heap);
 }
 
-void test_dequeuePriorityQueue(void)
+void test_enqueueMinHeap(void)
 {
-  int i;
+  int i, j;
   int nums[4][10] = {7, 3, 2, 1, 5, 6, 9, 4, 8, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
-  int expected[4][10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+  int expected[4][10] = {7, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
 
-  PriorityQueue_t *pq = newPriorityQueue();
-  for (i = 0; i < 10; i ++)
-    enqueuePriorityQueue(pq, nums[0][i], nums[0][i]);
-  for (i = 0; i < 10; i ++)
+  Heap_t *heap;
+  for (i = 0; i < 4; i ++)
   {
-    CU_ASSERT(maximum(pq) == expected[0][i]);
-    dequeuePriorityQueue(pq);
+    heap = createHeap(10, '-');
+    for (j = 0; j < 10; j ++)
+    {
+      enqueueHeap(heap, nums[i][j]);
+      CU_ASSERT(frontHeap(heap) == expected[i][j]);
+    }
+    destroyHeap(heap);
   }
-  destroyPriorityQueue(pq);
-
-  pq = newPriorityQueue();
-  for (i = 0; i < 10; i ++)
-    enqueuePriorityQueue(pq, nums[1][i], nums[1][i]);
-  for (i = 0; i < 10; i ++)
-  {
-    CU_ASSERT(maximum(pq) == expected[1][i]);
-    dequeuePriorityQueue(pq);
-  }
-  destroyPriorityQueue(pq);
-
-  pq = newPriorityQueue();
-  for (i = 0; i < 10; i ++)
-    enqueuePriorityQueue(pq, nums[2][i], nums[2][i]);
-  for (i = 0; i < 10; i ++)
-  {
-    CU_ASSERT(maximum(pq) == expected[2][i]);
-    dequeuePriorityQueue(pq);
-  }
-  destroyPriorityQueue(pq);
-
-  pq = newPriorityQueue();
-  for (i = 0; i < 10; i ++)
-    enqueuePriorityQueue(pq, nums[3][i], nums[3][i]);
-  for (i = 0; i < 10; i ++)
-  {
-    CU_ASSERT(maximum(pq) == expected[3][i]);
-    dequeuePriorityQueue(pq);
-  }
-  destroyPriorityQueue(pq);
 }
+
+void test_dequeueMinHeap(void)
+{
+  int i, j;
+  int nums[4][10] = {7, 3, 2, 1, 5, 6, 9, 4, 8, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+  int expected[4][10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+
+  Heap_t *heap;
+  for (i = 0; i < 4; i ++)
+  {
+    heap = createHeap(10, '-');
+    for (j = 0; j < 10; j ++)
+      enqueueHeap(heap, nums[i][j]);
+    for (j = 0; j < 10; j ++)
+    {
+      CU_ASSERT(frontHeap(heap) == expected[i][j]);
+      dequeueHeap(heap);
+    }
+    destroyHeap(heap);
+  }
+}
+
 
 int main()
 {
@@ -214,30 +122,29 @@ int main()
   if (CUE_SUCCESS != CU_initialize_registry())
   return CU_get_error();
 
-  /* add a suite to the registry */
-  pSuite = CU_add_suite("Enqueue Test", init_suite1, clean_suite1);
+  /* adds a suite to the registry */
+  pSuite = CU_add_suite("Enqueue Test", init_suit, clean_suite);
   if (NULL == pSuite) {
     CU_cleanup_registry();
     return CU_get_error();
   }
 
-  /* add the tests to the suite */
-  if ((NULL == CU_add_test(pSuite, "test of enqueueHeap()", test_enqueueHeap))
-  || (NULL == CU_add_test(pSuite, "test of enqueuePriorityQueue()", test_enqueuePriorityQueue)))
+  /* adds the tests to the suite */
+  if ((NULL == CU_add_test(pSuite, "test of enqueueMaxHeap()", test_enqueueMaxHeap)) ||
+      (NULL == CU_add_test(pSuite, "test of enqueueMinHeap()", test_enqueueMinHeap)))
   {
     CU_cleanup_registry();
     return CU_get_error();
   }
 
-
-  pSuite = CU_add_suite("Dequeue Test", init_suite1, clean_suite1);
+  pSuite = CU_add_suite("Dequeue Test", init_suit, clean_suite);
   if (NULL == pSuite) {
     CU_cleanup_registry();
     return CU_get_error();
   }
 
-  if ((NULL == CU_add_test(pSuite, "test of dequeueHeap()", test_dequeueHeap))
-  || (NULL == CU_add_test(pSuite, "test of dequeuePriorityQueue()", test_dequeuePriorityQueue)))
+  if ((NULL == CU_add_test(pSuite, "test of dequeueMaxHeap()", test_dequeueMaxHeap)) ||
+      (NULL == CU_add_test(pSuite, "test of dequeueMinHeap()", test_dequeueMinHeap)))
   {
     CU_cleanup_registry();
     return CU_get_error();
